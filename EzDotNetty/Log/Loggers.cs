478eccbdbcfde8759610304bd8744c8dc6c89b47
Elky (SeongHoon Kim) <elky84@gmail.com>
2022-01-05 15:26:08 +0900

    added missing logger file.
    added missing com.pfx file.

---
+++ EzDotNetty/Log/Loggers.cs
@@ -0,0 +1,25 @@
+﻿using Serilog;
+
+namespace EzDotNetty.Log
+{
+    public static class Loggers
+    {
+        public static Serilog.Core.Logger? Buff { get; set; }
+
+        public static Serilog.Core.Logger? Message { get; set; }
+
+        public static void Init()
+        {
+            Buff = New(Serilog.Events.LogEventLevel.Information);
+            Message = New(Serilog.Events.LogEventLevel.Information);
+        }
+
+        public static Serilog.Core.Logger New(Serilog.Events.LogEventLevel logEventLevel)
+        {
+            return new LoggerConfiguration()
+                .MinimumLevel.Is(logEventLevel)
+                .WriteTo.Console()
+                .CreateLogger();
+        }
+    }
+}
